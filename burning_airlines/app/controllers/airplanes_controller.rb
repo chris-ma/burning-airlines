a80586fd72ad1876dd3723de3951f4346a889608
Chris Ma <crispy-studios@hotmail.com>
2015-01-31 13:12:50 +1100

    created the CRUD for airplane

--- burning_airlines/app/controllers/airplanes_controller.rb
+++ burning_airlines/app/controllers/airplanes_controller.rb
@@ -3,4 +3,29 @@ class AirplanesController < ApplicationController
     @airplanes = Airplane.all
     render json: Airplane.all
   end
+
+  def create
+    @airplane = Airplane.create(airplane_params)
+    render json: @airplane
+  end
+
+  def update
+    @airplane = Airplane.find(params[:id])
+    @airplane.update(airplane_params)
+    render json: @airplane
+  end
+
+  def destroy
+     @airplane = Airplane.find(params[:id])
+     @airplane.destroy
+     render json: {status: 'ok'}
+    
+  end
+
+  private
+
+  def airplane_params
+    params.require(:airplane).permit(:name, :row, :column)
+    
+  end
 end
